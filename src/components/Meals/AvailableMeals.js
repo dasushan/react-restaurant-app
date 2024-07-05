9aaabf12fae1d8cf9e77e820c6a0648873aeb910
dasushan <53913896+dasushan@users.noreply.github.com>
2024-07-05 10:40:09 +0530

    updated meal-items style

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
+import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
-import MealItem from './MealItem'
+import MealItem from './MealItem/MealItem'
 
 const DUMMY_MEALS = [
     {
@@ -29,11 +30,14 @@ const DUMMY_MEALS = [
 ]
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => <MealItem name={meal.name} description={meal.description} price={meal.price}/>)
+    const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>)
 
     return (
         <div className={classes.meals}>
+            <Card>
             <ul>{mealsList}</ul>
+            </Card>
+            
         </div>
     )
 }
